f27975c27d12aa780f8967c596291fa3a984b978
Dario Beltran <DarioBeltran@MacBook-Air-de-Dario.local>
2015-07-14 20:47:06 -0500

    Creacion de archivo

---
+++ script.sh
@@ -0,0 +1,19 @@
+echo "Crear Carpeta"
+mkdir Mi_Carpeta
+echo "Entrando a la carpeta"
+cd Mi_Carpeta
+echo "Creando archivo"
+touch Mi_Archivo.txt
+echo "Saliendo de la carpeta"
+cd ..
+echo "Creando la carpeta Mi_Carpeta2"
+mkdir Mi_Carpeta2
+echo "Entrando a la carpeta2"
+cd Mi_Carpeta2
+echo "Creando archivo"
+touch Mi_Archivo2.txt
+echo "Saliendo de la carpeta"
+cd ..
+echo "Entrando a la carpeta"
+cd Mi_Carpeta
+echo "Todo bien."
